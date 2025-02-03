4e711344b7cb1ae9a607015aad63b1624421895f
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-03 11:44:33 -0500

    Add Flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,16 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import flowbitePlugin from 'flowbite/plugin'
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	content: ['./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+	darkMode: 'selector',
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, forms]
+	plugins: [typography, forms, flowbitePlugin]
 } satisfies Config;
