79fcbdb99f9fb51fa13c2a74c1f207f4f6fd48c9
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-03 12:40:16 -0500

    Basic login and register pages

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Label, Input, Button, Checkbox } from 'flowbite-svelte';
+</script>
+
+<div class="flex items-center justify-center h-screen bg-primary-100">
+	<form class="flex flex-col border-black space-y-3 w-11/12 md:w-96 bg-white shadow-lg
+	 shadow-primary-200 p-4 rounded-lg">
+		<h3 class="mb-4 text-xl font-medium">Sign in</h3>
+		<Label class="space-y-2">
+			<span>Email</span>
+			<Input type="email" name="email" placeholder="name@email.com" required />
+		</Label>
+		<Label class="space-y-2">
+			<span>Password</span>
+			<Input type="password" name="password" placeholder="•••••" required />
+		</Label>
+		<Checkbox>Remember me</Checkbox>
+		<Button>Sign In</Button>
+		<a href="/register" class="text-primary-700 hover:text-primary-700/80">
+			Don't have an account?
+		</a>
+	</form>
+</div>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Label, Input, Button, Checkbox } from 'flowbite-svelte';
+</script>
+
+<div class="flex items-center justify-center h-screen bg-primary-100">
+	<form class="flex flex-col border-black space-y-3 w-11/12 md:w-96 bg-white shadow-lg
+	shadow-primary-200 p-4 rounded-lg">
+		<h3 class="mb-4 text-xl font-medium">Sign up</h3>
+		<div class="flex space-x-3">
+			<Label class="space-y-2">
+				<span>First Name</span>
+				<Input type="text" name="firstName" placeholder="John" required />
+			</Label>
+			<Label class="space-y-2">
+				<span>Last Name</span>
+				<Input type="text" name="lastName" placeholder="Doe" required />
+			</Label>
+		</div>
+		<Label class="space-y-2">
+			<span>Email</span>
+			<Input type="email" name="email" placeholder="name@email.com" required />
+		</Label>
+		<Label class="space-y-2">
+			<span>Password</span>
+			<Input type="password" name="password" placeholder="•••••" required />
+		</Label>
+		<Checkbox>Remember me</Checkbox>
+		<Button>Sign Up</Button>
+		<a href="/login" class="text-primary-700 hover:text-primary-700/80">
+			Already have an account?
+		</a>
+	</form>
+</div>
