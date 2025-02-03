27b2cca2693a5f46ba0916d42000fb799f8bff40
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-03 09:50:47 -0500

    Add user and session table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,16 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	firstName: text('first_name').notNull(),
+	lastName: text('last_name').notNull(),
+	email: text('email').notNull().unique(),
+	password: text('password').notNull(),
 });
+
+export const session = sqliteTable('session', {
+	id: text('id').primaryKey(),
+	userId: integer('user_id').notNull()
+		.references(() => user.id),
+	expiresAt: integer('expires_at', {mode: 'timestamp'}).notNull(),
+})
