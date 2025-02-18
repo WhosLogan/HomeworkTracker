f37a815555ac9891905e5b7cbde15c89e0f15e34
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-18 17:21:43 -0500

    Basic dashboard pages

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<div class="flex">
+	<div class="h-screen flex">
+		<p>Homework Tracker</p>
+	</div>
+
+	{@render children()}
+</div>
