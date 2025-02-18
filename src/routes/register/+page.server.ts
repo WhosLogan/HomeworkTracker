ac2bedfedc677642f6e6da80a7abd79f6e17e0b3
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-18 17:22:35 -0500

    Finish register system

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,76 @@
+import { type Actions, fail, redirect } from '@sveltejs/kit';
+import z from "zod";
+import type { PageServerLoad } from './$types';
+import { setError, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+import { users } from '$lib/server/db/schema';
+import { hashPassword } from '$lib/server/auth/hashing';
+import { createSession, generateSecureToken, sessionName } from '$lib/server/auth/session';
+
+const registerSchema = z.object({
+	firstName: z.string().trim().min(2, "First Name is too short").max(20, "First Name is too long"),
+	lastName: z.string().trim().min(2, "Last Name is too short").max(20, "Last Name is too long"),
+	email: z.string().trim().toLowerCase()
+		.email("Email is invalid").max(100, "Email is too long"),
+	password: z.string().min(8, "Password is too short").max(128, "Password is too long"),
+});
+
+export const load: PageServerLoad = async () => {
+	const registerForm = await superValidate(zod(registerSchema));
+
+	return {
+		registerForm
+	}
+}
+
+// TODO: Add remember me functionality
+export const actions: Actions = {
+	default: async ({request, cookies}) => {
+		// Validate form
+		const registerForm = await superValidate(request,
+			zod(registerSchema));
+
+		if (!registerForm.valid) {
+			return fail(400, {registerForm});
+		}
+
+		// Try to fetch user
+		const exists = await db.query.users.findFirst({
+			columns: {
+				id: true,
+			},
+			where: eq(users.email, registerForm.data.email)
+		});
+
+		if (exists) {
+			return setError(registerForm, 'email', 'Email already exists.');
+		}
+
+		// Create user
+		const user = await db.insert(users).values({
+			firstName: registerForm.data.firstName,
+			lastName: registerForm.data.lastName,
+			email: registerForm.data.email,
+			password: hashPassword(registerForm.data.password),
+		}).returning();
+
+		// Create token
+		const token = generateSecureToken();
+
+		// Create session
+		const session = await createSession(token, user[0].id, false);
+
+		// Add the session
+		cookies.set(sessionName, token, {
+			httpOnly: true,
+			sameSite: "strict",
+			expires: session.expiresAt,
+			path: "/"
+		});
+
+		// Redirect to dashboard
+		redirect(302, "/dashboard");
+	}
+}
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,31 +1,53 @@
-<script>
-	import { Label, Input, Button, Checkbox } from 'flowbite-svelte';
+<script lang="ts">
+	import { Label, Input, Button, Checkbox, Helper } from 'flowbite-svelte';
+	import {superForm} from 'sveltekit-superforms';
+	import type {PageProps} from './$types';
+
+	const { data }: PageProps = $props();
+
+	const { form, enhance, errors } = superForm(data.registerForm);
 </script>
 
 <div class="flex items-center justify-center h-screen bg-primary-100">
 	<form class="flex flex-col border-black space-y-3 w-11/12 md:w-96 bg-white shadow-lg
-	shadow-primary-200 p-4 rounded-lg">
+	shadow-primary-200 p-4 rounded-lg" method="post" use:enhance>
 		<h3 class="mb-4 text-xl font-medium">Sign up</h3>
 		<div class="flex space-x-3">
 			<Label class="space-y-2">
 				<span>First Name</span>
-				<Input type="text" name="firstName" placeholder="John" required />
+				<Input type="text" name="firstName" placeholder="John" required
+							 bind:value={$form.firstName} />
+				{#if $errors.firstName}
+					<Helper color='red'>{$errors.firstName}</Helper>
+				{/if}
 			</Label>
 			<Label class="space-y-2">
 				<span>Last Name</span>
-				<Input type="text" name="lastName" placeholder="Doe" required />
+				<Input type="text" name="lastName" placeholder="Doe" required
+							 bind:value={$form.lastName} />
+				{#if $errors.lastName}
+					<Helper color='red'>{$errors.lastName}</Helper>
+				{/if}
 			</Label>
 		</div>
 		<Label class="space-y-2">
 			<span>Email</span>
-			<Input type="email" name="email" placeholder="name@email.com" required />
+			<Input type="email" name="email" placeholder="name@email.com" required
+						 bind:value={$form.email} />
+			{#if $errors.email}
+				<Helper color='red'>{$errors.email}</Helper>
+			{/if}
 		</Label>
 		<Label class="space-y-2">
 			<span>Password</span>
-			<Input type="password" name="password" placeholder="•••••" required />
+			<Input type="password" name="password" placeholder="•••••" required
+						 bind:value={$form.password} />
+			{#if $errors.password}
+				<Helper color='red'>{$errors.password}</Helper>
+			{/if}
 		</Label>
 		<Checkbox>Remember me</Checkbox>
-		<Button>Sign Up</Button>
+		<Button type="submit">Sign Up</Button>
 		<a href="/login" class="text-primary-700 hover:text-primary-700/80">
 			Already have an account?
 		</a>
