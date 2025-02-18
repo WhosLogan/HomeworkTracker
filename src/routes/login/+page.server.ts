aaceef971f79c21b7c2cfc78198a354df68ce105
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-18 17:43:39 -0500

    Finish login page

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,64 @@
+import type { PageServerLoad } from './$types';
+import { setError, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import z from "zod";
+import { type Actions, fail, redirect } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
+import { users } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
+import { verifyPassword } from '$lib/server/auth/hashing';
+import { createSession, generateSecureToken, sessionName } from '$lib/server/auth/session';
+
+const loginSchema = z.object({
+	email: z.string().trim().toLowerCase()
+		.email("Email is invalid").max(100, "Email is too long"),
+	password: z.string().min(8, "Password is too short").max(128, "Password is too long"),
+});
+
+export const load: PageServerLoad = async () => {
+	const loginForm = await superValidate(zod(loginSchema));
+
+	return {loginForm};
+}
+
+export const actions: Actions = {
+	default: async ({request, cookies}) => {
+		const form =
+			await superValidate(request, zod(loginSchema));
+
+		if (!form.valid) {
+			return fail(400, {form});
+		}
+
+		const user = await db.query.users.findFirst({
+			where: eq(users.email, form.data.email)
+		});
+
+		if (!user) {
+			setError(form, 'password', 'Invalid Email or Password');
+			return setError(form, 'email', 'Invalid Email or Password');
+		}
+
+		if (!verifyPassword(form.data.password, user.password)) {
+			setError(form, 'password', 'Invalid Email or Password');
+			return setError(form, 'email', 'Invalid Email or Password');
+		}
+
+		// Create token
+		const token = generateSecureToken();
+
+		// Create session
+		const session = await createSession(token, user.id, false);
+
+		// Add the session
+		cookies.set(sessionName, token, {
+			httpOnly: true,
+			sameSite: "strict",
+			expires: session.expiresAt,
+			path: "/"
+		});
+
+		// Redirect to dashboard
+		redirect(302, "/dashboard");
+	}
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,35 @@
-<script>
-	import { Label, Input, Button, Checkbox } from 'flowbite-svelte';
+<script lang="ts">
+	import { Label, Input, Button, Checkbox, Helper } from 'flowbite-svelte';
+	import { superForm } from 'sveltekit-superforms';
+	import type {PageProps} from "./$types";
+
+	const {data}: PageProps = $props();
+
+	const {form, enhance, errors} = superForm(data.loginForm);
 </script>
 
 <div class="flex items-center justify-center h-screen bg-primary-100">
 	<form class="flex flex-col border-black space-y-3 w-11/12 md:w-96 bg-white shadow-lg
-	 shadow-primary-200 p-4 rounded-lg">
+	 shadow-primary-200 p-4 rounded-lg" method="post" use:enhance>
 		<h3 class="mb-4 text-xl font-medium">Sign in</h3>
 		<Label class="space-y-2">
 			<span>Email</span>
-			<Input type="email" name="email" placeholder="name@email.com" required />
+			<Input type="email" name="email" placeholder="name@email.com" required
+						 bind:value={$form.email}/>
+			{#if $errors.email}
+				<Helper color='red'>{$errors.email}</Helper>
+			{/if}
 		</Label>
 		<Label class="space-y-2">
 			<span>Password</span>
-			<Input type="password" name="password" placeholder="•••••" required />
+			<Input type="password" name="password" placeholder="•••••" required
+						 bind:value={$form.password} />
+			{#if $errors.password}
+				<Helper color='red'>{$errors.password}</Helper>
+			{/if}
 		</Label>
 		<Checkbox>Remember me</Checkbox>
-		<Button>Sign In</Button>
+		<Button type="submit">Sign In</Button>
 		<a href="/register" class="text-primary-700 hover:text-primary-700/80">
 			Don't have an account?
 		</a>
