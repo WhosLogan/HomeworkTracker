4f1979ce0d3c9f0009676623c0c7f30a8179245f
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-24 10:46:59 -0500

    Return courses in layout server

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -1,7 +1,53 @@
 import type { LayoutServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+import { assignments, courses } from '$lib/server/db/schema';
+
+type Assignment = {
+	id: number;
+	name: string;
+	dueDate: Date;
+}
+
+type Course = {
+	id: number;
+	name: string;
+	professor: string|null;
+	assignments: Assignment[];
+}
 
 export const load: LayoutServerLoad = async ({locals}) => {
+	const userCourses = await db.query.courses.findMany({
+		where: eq(courses.userId, locals.user!.id)
+	});
+
+	const filteredCourses: Course[] = [];
+
+	for (const course of userCourses) {
+		const filteredCourse: Course = {
+			id: course.id,
+			name: course.name,
+			professor: course.professorName,
+			assignments: []
+		}
+
+		const userAssignments = await db.query.assignments.findMany({
+			where: eq(assignments.courseId, course.id),
+		});
+
+		for (const assignment of userAssignments) {
+			filteredCourse.assignments.push({
+				id: assignment.id,
+				name: assignment.assignmentName,
+				dueDate: assignment.dueDate
+			})
+		}
+
+		filteredCourses.push(filteredCourse);
+	}
+
 	return {
-		user: locals.user
+		user: locals.user,
+		courses: filteredCourses
 	}
 }
