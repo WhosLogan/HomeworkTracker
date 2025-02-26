001f723022ac543931011e69efa90e7b706f8446
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-02-26 01:06:46 -0500

    Assignments can be created and viewed per course

--- src/routes/dashboard/courses/[id]/+page.server.ts
+++ src/routes/dashboard/courses/[id]/+page.server.ts
@@ -1,8 +1,17 @@
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { error, fail, redirect } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
 import { and, eq } from 'drizzle-orm';
-import { courses } from '$lib/server/db/schema';
+import { assignments, courses } from '$lib/server/db/schema';
+import z from 'zod';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+const addAssignmentSchema = z.object({
+	assignmentName: z.string().min(3, "Assignment Name must be at least 3 characters")
+		.max(40, "Assignment Name must not exceed 40 characters"),
+	dueDate: z.date({required_error: "Due date is required"}),
+});
 
 export const load: PageServerLoad = async ({locals, params}) => {
 	if (isNaN(Number(params.id))) {
@@ -17,5 +26,35 @@ export const load: PageServerLoad = async ({locals, params}) => {
 		redirect(303, '/dashboard');
 	}
 
-	return {course};
+	const allAssignments = await db.query.assignments.findMany({
+		where: eq(assignments.courseId, course.id)
+	});
+
+	const addAssignmentForm = await superValidate(zod(addAssignmentSchema));
+
+	return {course, addAssignmentForm, assignments: allAssignments};
+}
+
+export const actions: Actions = {
+	addAssignment: async ({request, locals, params}) => {
+		const form = await superValidate(request, zod(addAssignmentSchema));
+
+		if (!form.valid) {
+			return fail(400, {form});
+		}
+
+		const course = await db.query.courses.findFirst({
+			where: and(eq(courses.id, parseInt(params.id)), eq(courses.userId, locals.user!.id))
+		});
+
+		if (!course) {
+			error(400, "Invalid request");
+		}
+
+		await db.insert(assignments).values({
+			courseId: course.id,
+			assignmentName: form.data.assignmentName,
+			dueDate: form.data.dueDate
+		});
+	}
 }
--- src/routes/dashboard/courses/[id]/+page.svelte
+++ src/routes/dashboard/courses/[id]/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import {
+		Button,
+		Helper,
+		Input,
+		Label,
+		Modal,
+		Table,
+		TableBody, TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+	import { superForm } from 'sveltekit-superforms';
 
 	const {data}: PageProps = $props();
+
+	let addMenu = $state(false);
+
+	const {enhance: addAssignmentEnhance, form: addAssignmentForm, errors: addAssignmentErrors} = superForm(data.addAssignmentForm, {
+		onResult: async ({result}) => {
+			if (result.type === 'success') addMenu = false;
+		}
+	});
 </script>
 
 <div class="flex flex-col items-center">
@@ -9,4 +30,50 @@
 	{#if data.course.professorName}
 		<p>Professor: {data.course.professorName}</p>
 	{/if}
+</div>
+
+<!-- Assignment Display -->
+
+{#if data.assignments.length > 0}
+	<Table class="mt-10">
+		<TableHead>
+			<TableHeadCell>Assignment Name</TableHeadCell>
+			<TableHeadCell>Due Date</TableHeadCell>
+		</TableHead>
+		<TableBody>
+			{#each data.assignments as assignment}
+				<TableBodyRow>
+					<TableBodyCell>{assignment.assignmentName}</TableBodyCell>
+					<TableBodyCell>{assignment.dueDate.toDateString()}</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		</TableBody>
+	</Table>
+{:else}
+	<h2 class="text-center">Add an assignment to get started</h2>
+{/if}
+
+<Modal title="Add Assignment" bind:open={addMenu}>
+	<form action="?/addAssignment" method="post" class="flex flex-col space-y-5 overflow-visible" use:addAssignmentEnhance>
+		<Label class="space-y-2 w-full">
+			<span>Assignment Name</span>
+			<Input type="text" name="assignmentName" placeholder="My Assignment" required bind:value={$addAssignmentForm.assignmentName} />
+					{#if $addAssignmentErrors.assignmentName}
+						<Helper color='red'>{$addAssignmentErrors.assignmentName}</Helper>
+					{/if}
+		</Label>
+		<Label class="space-y-2 w-full">
+			<span>Due Date</span>
+			<Input name="dueDate" required type="date" bind:value={$addAssignmentForm.dueDate} />
+					{#if $addAssignmentErrors.professorName}
+						<Helper color='red'>{$addAssignmentErrors.dueDate}</Helper>
+					{/if}
+		</Label>
+		<Button variant="primary" type="submit">Create Assignment</Button>
+	</form>
+</Modal>
+
+<!-- Add Assignment Button-->
+<div class="fixed bottom-5 right-5">
+	<Button onclick={() => addMenu = true}>Add Assignment</Button>
 </div>
