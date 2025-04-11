5253fee089cdd05ecd37f38caba94a7c99b01fcc
mkollmeyer <max.kollmeyer@gmail.com>
2025-04-11 12:07:15 -0400

    Worked on unit tests in new branch

--- src/tests/session.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { generateSecureToken } from '$lib/server/auth/hashing';
-
-describe('verify test1', () => {
-	it('tests verifyPassword for true', () => {
-		expect(verifyPassword('password', hashPassword('password'))).toBe(true);
-	});
-});
