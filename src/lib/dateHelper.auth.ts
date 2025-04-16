b8413fd04da221f9d4878e4377df83e81850b445
mkollmeyer <max.kollmeyer@gmail.com>
2025-04-15 21:34:26 -0400

    updated dateHelper.auth.ts

--- src/lib/dateHelper.auth.ts
+++ src/lib/dateHelper.auth.ts
@@ -1,9 +1,9 @@
 import { describe, it, expect } from 'vitest';
 import { toUTCDateString } from '$lib/server/dateHelper'
 
+const date = new Date(2025, 3, 16, 10, 0, 0, 0);
+
 describe('test string convert', () => {
-    const utcTime = '2025-4-16T01:00:00.000z';
-    const date = new Date(utcTime);
 	test('tests toUTCDateString', () => {
 		expect(toUTCDateString(date)).toBe('Wed Apr 16 2025');
 	});
